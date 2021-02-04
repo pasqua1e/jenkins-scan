56abcc31b067372c9797774d3459cb805da8c108
pasqua1e <31853954+pasqua1e@users.noreply.github.com>
2021-02-04 19:06:14 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,12 @@
-
+provider "aws" {
+    region = "eu-west-2"
+}
+resource "aws_iam_account_password_policy" "strict" {
+  minimum_password_length        = 15
+  require_lowercase_characters   = true
+  require_numbers                = true
+  require_uppercase_characters   = true
+  require_symbols                = true
+  allow_users_to_change_password = true
+  max_password_age               = 92
+}
