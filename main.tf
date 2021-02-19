bc4880a8462efd639decf99c0c669f5b9e24f899
pasqua1e <31853954+pasqua1e@users.noreply.github.com>
2021-02-19 18:54:00 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,5 +8,5 @@ resource "aws_iam_account_password_policy" "strict" {
   require_uppercase_characters   = true
   require_symbols                = true
   allow_users_to_change_password = true
-  max_password_age               = 93
+  max_password_age               = 94
 }
